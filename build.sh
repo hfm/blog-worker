5cea59fe2810233eac2b2cd98fcc1e2ab576700f
Okumura Takahiro <hfm.garden@gmail.com>
2014-03-02 00:19:19 +0900

    fix

--- build.sh
+++ build.sh
@@ -1,21 +1,25 @@
 #!/bin/bash 
-set -eu
+set -e
 
-export LANG=en_US.UTF-8
-export LANGUAGE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
+PATH=/usr/local/ruby-2.1.0/bin:$PATH
+LANG=ja_JP.UTF-8
+LANGUAGE=ja_JP.UTF-8
+LC_ALL=ja_JP.UTF-8
 
 GIT_LOCATION=/usr/local/src/blog
-REPO=git@github.com:Tacahilo/blog.git
+REPO=git@github.com:tacahilo/blog.git
 WWW_LOCATION=/var/www/blog
 BUNDLE_GEMFILE=$GIT_LOCATION/Gemfile
 
 # update_repo
-[ -d $GIT_LOCATION ] || mkdir -p $GIT_LOCATION
+[ -d $GIT_LOCATION ]      || mkdir -p $GIT_LOCATION
 [ -d $GIT_LOCATION/.git ] || git clone $REPO $GIT_LOCATION
 cd $GIT_LOCATION && git pull origin master
 
 # build and sync
-[ -d ./.bundle ] || bundle install --path vendor
+bundle install --path vendor
+bundle update
 bundle exec rake build
-rsync -a --delete $GIT_LOCATION/_site/ $WWW_LOCATION
+rsync -az --delete $GIT_LOCATION/_site/ $WWW_LOCATION
+
+varnishadm -T 127.0.0.1:6082 -S /etc/varnish/secret purge.url .
